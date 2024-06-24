54756c86609f5ce90e383fb594ffa8b974d013eb
arramosm13 <arramosm@unjbg.edu.pe>
2024-06-23 22:34:11 -0500

    Actualizacion del ejercicio 2: agregue un do-while para que se pueda hacer la comparacion que indique que son numeros primos, solo a los numeros que son mayor a 0.

--- EJERCICIO2.cpp
+++ EJERCICIO2.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 int main(){
     int n1,n2,c=0, i=1;
-    
+    do{
 	system("cls");
     cout<<"Ingrese el primer numero:";cin>>n1;
     cout<<"Ingrese el segundo numero:";cin>>n2;
     	while(i<=n1 && i<=n2){
         	if(n1%i==0 && n2%i==0){
-            	c++;
+            c++;
         }
         i++;
     	}
@@ -20,6 +20,8 @@ int main(){
 		if (c>1 && c!=0){
 			cout<<"Los numeros no son PESI.";
 			system("pause");
-		}return 0;
+		}
+	} while(n1>0 && n2>0);
+	cout<< "Se cerrara el programa."<<endl; 
+	return 0;
  	} 
- 	
