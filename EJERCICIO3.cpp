e4856ffcf0f3dd7183bdec3eb8b7c3421ce765df
arramosm13 <arramosm@unjbg.edu.pe>
2024-06-23 21:53:03 -0500

    Agregando el ejercicio 3

---
+++ EJERCICIO3.cpp
@@ -0,0 +1,28 @@
+//nombre:Ramos Mamani Alejandra Del Rosario
+#include <iostream>
+using namespace std;
+float suma(int);
+int factorial(int);
+int main(){
+    int n;
+    float sum;
+	cout<<"Ingrese el numero:";cin>>n;
+    	sum=suma(n);
+    	cout<<"La sumatoria total es:"<<sum;
+}		
+	
+float suma(int a){
+    float s=0;
+    for(int i=1; i<=a; i++){
+        s = s + (float(factorial(i)) / (2*i));
+    }
+    return s;
+}
+
+int factorial(int b){
+    if (b != 0){
+        return b * factorial(b-1);
+    } else{
+        return 1;
+	}
+}
